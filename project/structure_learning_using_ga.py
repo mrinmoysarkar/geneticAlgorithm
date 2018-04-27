710c125e33fc5a3150cb2d7ec8a88e2e008f21f2
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2018-04-27 18:10:57 -0400

    ga is aplied to learn structure
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- project/structure_learning_using_ga.py
+++ project/structure_learning_using_ga.py
@@ -13,7 +13,64 @@ from pgmpy.estimators import MaximumLikelihoodEstimator, BayesianEstimator
 from pgmpy.estimators import K2Score, BicScore, BdeuScore
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
+
+def generate_pop(size,strLen):
+    pop = np.random.rand(size,strLen)
+    pop = np.round(pop)
+    return pop
+
+def get_structure_from_string(string,var):
+    indx = 0
+    structure = []
+    for i in range(len(var)-1):
+        for j in range(i+1):
+            if string[indx] == 1:
+                topl = (var[j],var[i+1])
+                structure.append(topl)
+            indx += 1;
+    model = BayesianModel(structure)
+    return model
+
+def selection(pop,popSize,var,scoreFunc):
+    score=np.zeros(popSize)
+    for i in range(popSize):
+        model = get_structure_from_string(pop[i],var)
+        score[i] = scoreFunc.score(model)
+    avgScore = np.mean(score)
+    noOfsamples = 10;
+    indexs = np.zeros(noOfsamples,dtype=int);
+    for i in range(noOfsamples):
+        indexs[i] = np.argmax(score)
+        mi = np.min(score)
+        score[indexs[i]] = mi
+    i1 = np.random.randint(low=0,high=noOfsamples-1)
+    p1 = indexs[i1]
+    indexs = np.delete(indexs,i1);
+    i2 = np.random.randint(low=0,high=noOfsamples-2)
+    p2 = indexs[i2]
+    return p1,p2,avgScore
+        
+    
+
+def crossover(pop,p1,p2,pc,stringLen):
+    crossSite = np.random.randint(low=1,high=stringLen-2)
+    if np.random.rand() > pc:
+        temp = pop[p1,:]
+        pop[p1,crossSite:stringLen] = pop[p2,crossSite:stringLen]
+        pop[p2,crossSite:stringLen] = temp[crossSite:stringLen]
+    return pop
+
+def mutation(pop,pm,popSize,strigLen):
+    for i in range(popSize):
+        for j in range(stringLen):
+            if np.random.rand() > pm:
+                if pop[i,j] == 1: 
+                    pop[i,j] = 0;
+                else:
+                    pop[i,j] = 1;
+    return pop
 
 if __name__ == '__main__':
     bnNetworkFileName = 'asia.bif'
@@ -21,7 +78,7 @@ if __name__ == '__main__':
     var = bn.get_variables()
     bnModel = bn.get_model()
     inference = BayesianModelSampling(bnModel)
-    dataSet = inference.forward_sample(size=1000, return_type='dataframe')
+    dataSet = inference.forward_sample(size=500, return_type='dataframe')
     
     model = BayesianModel()
     model.add_nodes_from(var)
@@ -40,11 +97,36 @@ if __name__ == '__main__':
     bic = BicScore(dataSet)
     bdeu = BdeuScore(dataSet)
     
-    print(k2.score(bnModel))
-    print(k2.score(model))
+#    print(k2.score(bnModel))
+#    print(k2.score(model))
+#    
+#    print(bic.score(bnModel))
+#    print(bic.score(model))
+#    
+#    print(bdeu.score(bnModel))
+#    print(bdeu.score(model))
     
-    print(bic.score(bnModel))
-    print(bic.score(model))
+    noOfvar = len(var)
+    popSize = 100;
+    noOfgeneration = 1000
+    pm = 0.001
+    pc = 0.6
+    stringLen = int(noOfvar*(noOfvar-1)/2)
     
-    print(bdeu.score(bnModel))
-    print(bdeu.score(model))
+    pop = generate_pop(popSize,stringLen)
+    #model = get_structure_from_string(pop[1],var)
+    
+    #print(k2.score(model))
+    #model.fit(dataSet, estimator=MaximumLikelihoodEstimator)
+    #print(k2.score(model))
+    #crossover(pop,1,2,0.6,stringLen)
+    scores = np.zeros(noOfgeneration)
+    for i in range(noOfgeneration):
+        p1,p2,avgScore = selection(pop,popSize,var,k2)
+        scores[i] = avgScore
+        pop = crossover(pop,p1,p2,pc,stringLen)
+        pop = mutation(pop,pm,popSize,stringLen)
+        print("generationNo.: ",i)
+    #print(p1,p2,avgScore)
+    plt.plot(scores)
+    plt.show()
